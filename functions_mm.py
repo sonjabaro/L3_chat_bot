a02f9435a4871f0ecb1bcb8d896a62a4c791d118
mmccanse <148015402+mmccanse@users.noreply.github.com>
2024-04-16 11:09:52 -0600

    functions.py cleanup

--- functions_mm.py
+++ functions_mm.py
@@ -20,6 +20,7 @@ from langchain.chains.question_answering import load_qa_chain
 # Import RegEx for translate function, to split sentences in avoiding token limits
 import re
 
+#Get keys #########################################################################################
 load_dotenv()
 
 # Set the model name for our LLMs.
@@ -27,47 +28,8 @@ OPENAI_MODEL = "gpt-3.5-turbo"
 # Store the API key in a variable.
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-
-#Setting the Chatbot Model
-
-# Set the model name for our LLMs.
-OPENAI_MODEL = "gpt-3.5-turbo"
-# Store the API key in a variable.
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-#Instantiating the llm we'll use and the arguments to pass
-llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL, temperature=0.0)
-
-# Define the wikipedia topic as a string.
-wiki_topic = "diabetes"
-
-# Load the wikipedia results as documents, using a max of 2.
-#included error handling- unable to load documents
-try:
-    documents = WikipediaLoader(query=wiki_topic, load_max_docs=2, load_all_available_meta=True).load()
-except Exception as e:
-    print("Failed to load documents:", str(e))
-    documents = []
-# Create the QA chain using the LLM.
-chain = load_qa_chain(llm)
-
-#Define the function to call the LLM
-def handle_query(user_query):
-    if not documents:
-        return "Source not loading info; please try again later."
-
-    if user_query.lower() == 'quit':
-        return "Goodbye!"
-
-    try:
-        # Pass the documents and the user's query to the chain, and return the result.
-        result = chain.invoke({"input_documents": documents, "question": user_query})
-        return result["output_text"] if result["output_text"].strip() else "No answer found, try a different question."
-        
-    except Exception as e:
-        return "An error occurred while searching for the answer: " + str(e)
-    
 #Define language variables ###################################################################################
+
 #Define voice map
 voice_map = {
     "ar": "Hala",
@@ -107,6 +69,48 @@ languages = gr.Dropdown(
 default_language = "English"
 
 #############################################################################################################
+
+#Setting the Chatbot Model
+
+# Set the model name for our LLMs.
+OPENAI_MODEL = "gpt-3.5-turbo"
+# Store the API key in a variable.
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+#Instantiating the llm we'll use and the arguments to pass
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL, temperature=0.0)
+
+# Define the wikipedia topic as a string.
+wiki_topic = "diabetes"
+
+# Load the wikipedia results as documents, using a max of 2.
+#included error handling- unable to load documents
+try:
+    documents = WikipediaLoader(query=wiki_topic, load_max_docs=2, load_all_available_meta=True).load()
+except Exception as e:
+    print("Failed to load documents:", str(e))
+    documents = []
+# Create the QA chain using the LLM.
+chain = load_qa_chain(llm)
+
+##############################################################################################################
+
+#Define the function to call the OpenAI chat LLM
+def handle_query(user_query):
+    if not documents:
+        return "Source not loading info; please try again later."
+
+    if user_query.lower() == 'quit':
+        return "Goodbye!"
+
+    try:
+        # Pass the documents and the user's query to the chain, and return the result.
+        result = chain.invoke({"input_documents": documents, "question": user_query})
+        return result["output_text"] if result["output_text"].strip() else "No answer found, try a different question."
+        
+    except Exception as e:
+        return "An error occurred while searching for the answer: " + str(e)
+    
 
 #Define function to transcribes audio to text using Whisper in the original language it was spoken
 def transcribe_audio_original(audio_filepath):
